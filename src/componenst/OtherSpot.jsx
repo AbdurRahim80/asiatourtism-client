d67069eb507baf164df0f03eb90a9639d5e58330
AbdurRahim80 <mdrahim.nil2005@gmail.com>
2024-08-24 12:16:49 -0700

    add otherSpot

---
+++ src/componenst/OtherSpot.jsx
@@ -0,0 +1,71 @@
+
+const OtherSpot = () => {
+    return (
+        <div>
+            <section className="py-6 sm:py-12 dark:bg-gray-100 dark:text-gray-800">
+                <div className="container p-6 mx-auto space-y-8">
+                    <div className="space-y-2 text-center">
+                        <h2 className="text-3xl font-bold uppercase">Others relatives Tourist spot</h2>
+                        <p className="font-serif text-sm dark:text-gray-600">Let's go beautiful tourist spot.</p>
+                    </div>
+                    <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
+                        <article className="flex flex-col dark:bg-gray-50">
+                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+                                <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://i.ibb.co/Kz2YV4v/Phuket.jpg" />
+                            </a>
+                            <div className="flex flex-col flex-1 p-6">
+                                <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
+                                <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600">Cox’s Bazar</a>
+                                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Unwind at the World's Longest Beach: Cox's Bazar</h3>
+                                <p>Cox's Bazar offers the world’s longest natural sea beach, serene landscapes, and stunning sunsets. Perfect for relaxation.</p>
+                                
+                            </div>
+                        </article>
+                        <article className="flex flex-col dark:bg-gray-50">
+                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+                                <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://i.ibb.co/0Z5CLQr/cox-s-bazar.jpg" />
+                            </a>
+                            <div className="flex flex-col flex-1 p-6">
+                                <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
+                                <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600">Phuket</a>
+                                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">TExperience the Vibrant Island Life in Phuket, Thailand</h3>
+                                <p>Phuket is Thailand’s tropical paradise, famous for its beaches, nightlife, cultural heritage, and adventure-filled activities.</p>
+                               
+                            </div>
+                        </article>
+                        <article className="flex flex-col dark:bg-gray-50">
+                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+                                <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://i.ibb.co/WGmNDs7/penang.jpg" />
+                            </a>
+                            <div className="flex flex-col flex-1 p-6">
+                                <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
+                                <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600">Penang</a>
+                                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Cultural and Culinary Delights Await in Penang, Malaysia</h3>
+                                <p>Penang, Malaysia, is a cultural hub known for its historic sites, colonial architecture, and world-renowned street food.</p>
+                                
+                            </div>
+                        </article>
+                        <article className="flex flex-col dark:bg-gray-50">
+                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+                                <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://i.ibb.co/TPBdvhY/komodo.jpg" />
+                            </a>
+                            <div className="flex flex-col flex-1 p-6">
+                                <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
+                                <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600">Komodo</a>
+                                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Adventure Awaits on Komodo Island, Indonesia</h3>
+                                <p>Komodo Island is famous for its legendary Komodo dragons, diverse wildlife, and stunning marine life, ideal for explorers.</p>
+                               
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default OtherSpot;
+
+
+
+
